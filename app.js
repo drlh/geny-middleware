f093f8d394cf617f5d2de7658c046d2387ff94de
a571568 <a571568@MC0Y35VC.ww930.my-it-solutions.net>
2016-10-05 18:34:31 +0200

    Database prepared
    - added necessary modules
    - removed node modules

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 "use strict"
 
 // Modules
-var express = require('express'), logger = require('morgan'), session = require('express-session');
+var express = require('express');
+var logger = require('morgan');
+var session = require('express-session');
 var fs = require("fs");
 
-var Grant = require('grant-express'), grant = new Grant(
-		require('./config.json')), oauthSignature = require('oauth-signature');
+var Grant = require('grant-express');
+var grant = new Grant(require('./config.json'));
 
+var mysql = require('mysql');
+
+
+// MODULES
+
+// VARIABLES
 var oauth_var = null;
 var signature_parameters = null;
+var url_frontend = "http://localhost:8080";
+var dbconnection = mysql.createConnection({
+	host : "localhost",
+	user : "root",
+	password : "root",
+	database : "geny"
+});
 
 // Initialize express
 var app = express()
@@ -22,108 +37,68 @@ app.use(session({
 app.use(grant);
 
 app.get('/handle_xing_callback', function(req, res) {
-	
+
 	var ouath_authentication = req.query;
 	createKeys(ouath_authentication);
-	res.end(JSON.stringify(req.query, null, 2));
+	res.redirect('/sign');
 });
 app.get('/sign', function(req, res) {
-	
-//	console.log
-//	var query = req.query;
-//	console.log(query.url);
-	
+
 	var url = "https://api.xing.com/v1"
 	var s = createSignaturePlain();
-	
+
 	var data = {
-			signature: s,
-			token : signature_parameters,
-			oauth : oauth_var
+		signature : s,
+		token : signature_parameters,
+		oauth : oauth_var
 	}
 	res.end(JSON.stringify(data, null, 2));
 });
 
 app.get('/logout', function(req, res) {
-	req.session = null;
 	oauth_var = null;
-	signature = null;
 	signature_parameters = null;
-	req.session.destroy();
+	req.session = null;
+
+	res.redirect(url_frontend);
 });
 
 app.listen(3000, function() {
-	console.log('Express server listening on port ' + 3000)
+	console.log('Express server listening on port ' + 3000);
 });
 
 // FUNCTIONS
-function createSignature(url){
-	var httpMethod = 'GET';
-	if(signature_parameters != null && oauth_var != null){
-		var s = oauthSignature.generate(httpMethod, url, signature_parameters, oauth_var.consumer_secret ,oauth_var.oauth_token_secret);
-		console.log(s);
-		return s;
-	}
-	
-	return "error in calculating a signature";
-};
 
-function createSignaturePlain(){
-	if(signature_parameters != null && oauth_var != null){
-		
+function createSignaturePlain() {
+	if (signature_parameters != null && oauth_var != null) {
+
 		console.log(oauth_var);
 		console.log(signature_parameters);
-		
-		return oauth_var.consumer_secret +"%26"+oauth_var.oauth_token_secret;
+
+		return oauth_var.consumer_secret + "%26" + oauth_var.oauth_token_secret;
 	}
 	return "error in calculating a signature";
 };
 
 function createKeys(ouath_authentication) {
 
-//	console.log(getAppkeys());
-//	
-//	var xingApp = getAppkeys();
 	signature_parameters = {
-//			consumer_key : xingApp.consumer_key,
-			consumer_key : "fd305e7cfa9ce4a82581",
-			oauth_token : ouath_authentication.access_token,
-			oauth_nonce : getNonce(),
-	        oauth_signature_method : 'PLAINTEXT',
-	        oauth_timestamp : getTimestamp(),
-	        oauth_version : '1.0'
+		consumer_key : "fd305e7cfa9ce4a82581",
+		oauth_token : ouath_authentication.access_token,
+		oauth_nonce : getNonce(),
+		oauth_signature_method : 'PLAINTEXT',
+		oauth_timestamp : getTimestamp(),
+		oauth_version : '1.0'
 	};
-	
+
 	oauth_var = {
-//			consumer_key : xingApp.consumer_key,
-			consumer_key : "fd305e7cfa9ce4a82581",
-//			consumer_secret : xingApp.consumer_secret,
-			consumer_secret : "071018184da8093ecadaec78ac251963c9a39b9c",
-			oauth_token : ouath_authentication.access_token,
-			oauth_token_secret : ouath_authentication.access_secret
+		consumer_key : "fd305e7cfa9ce4a82581",
+		consumer_secret : "071018184da8093ecadaec78ac251963c9a39b9c",
+		oauth_token : ouath_authentication.access_token,
+		oauth_token_secret : ouath_authentication.access_secret
 	};
 
 };
-
-//function getAppkeys() {
-//	var d = null;
-//	fs.readFileSync('config.json', 'utf-8' ,function(err, data) {
-//		if (err) {
-//			console.log(err);
-//			process.exit(1);
-//		}
-//		
-//		console.log(data);
-//		
-//		data = JSON.parse(data);
-//		d = {
-//				consumer_key : data.xing.key,
-//				consumer_secret : data.xing.secret
-//		};
-//		console.log("d:"+JSON.stringify(d));
-//		return d;
-//	});
-//};
 
 function getNonce() {
 	return Math.random().toString(36).substring(5);
